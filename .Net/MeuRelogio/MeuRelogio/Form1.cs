fb98aed14d44ab1848f0ceb049da64f97a2362c0
Código, Hardware e Cia <codigohardwareecia@hotmail.com>
2026-03-06 15:37:45 -0300

    Adicionado os exemplos de código utilizado no Canal do Youtube

---
+++ .Net/MeuRelogio/MeuRelogio/Form1.cs
@@ -0,0 +1,15 @@
+namespace MeuRelogio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString("HH:mm:ss:fff");
+        }
+    }
+}
